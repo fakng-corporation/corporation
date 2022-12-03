956f1a7370dabce922c63784e0415eded63c752c
mighty-mhsl <mihusle@protonmail.com>
2022-12-03 20:11:34 +0200

    Initial commit

---
+++ src/main/java/com/corporation/corporation/CorporationApplication.java
@@ -0,0 +1,13 @@
+package com.corporation.corporation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporationApplication.class, args);
+	}
+
+}
