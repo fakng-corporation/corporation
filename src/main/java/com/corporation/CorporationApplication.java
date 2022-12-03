7e6bf3ff5fe78af5c74cc1e060200006cf8afa3e
mighty-mhsl <mihusle@protonmail.com>
2022-12-03 20:43:23 +0200

    Added docker compose file for local db start up

--- src/main/java/com/corporation/CorporationApplication.java
+++ src/main/java/com/corporation/CorporationApplication.java
@@ -1,4 +1,4 @@
-package com.corporation.corporation;
+package com.corporation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
