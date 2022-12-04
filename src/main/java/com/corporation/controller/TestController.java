b24ff248de3cc2084c3d715de4d9c6756cf942d3
mighty-mhsl <mihusle@protonmail.com>
2022-12-04 18:31:13 +0200

    test commit

---
+++ src/main/java/com/corporation/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.corporation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/user")
+    public String getUser() {
+        return "Vlad Mishustin";
+    }
+}
