fecc2d223edd1f0d8a973f396111391ea47b08d2
Zavulon1747 <d1mas322@yandex.ru>
2022-12-16 01:36:17 +0400

    Переименовал user_id в fk_user_id

--- src/main/resources/db/changelog/init-post.sql
+++ src/main/resources/db/changelog/init-post.sql
@@ -2,9 +2,9 @@ CREATE TABLE post (
     id  BIGSERIAL PRIMARY KEY,
     title varchar(128) not null,
     description varchar(10240) not null,
-    user_id bigint,
+    fk_user_id bigint,
     published boolean,
     created_at timestamptz DEFAULT current_timestamp,
     updated_at timestamptz DEFAULT current_timestamp,
-    FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE
+    FOREIGN KEY (fk_user_id) REFERENCES "user"(id) ON DELETE CASCADE
 )
