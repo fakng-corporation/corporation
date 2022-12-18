b93a53c2883d303c4bf99b64ba69485a73080209
Zavulon1747 <d1mas322@yandex.ru>
2022-12-19 00:51:11 +0400

    Добавлены аннотации в PostMapper

--- src/main/java/com/corporation/mapper/PostMapper.java
+++ src/main/java/com/corporation/mapper/PostMapper.java
@@ -4,10 +4,13 @@ import com.corporation.dto.PostDto;
 import com.corporation.model.Post;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.FIELD)
 public interface PostMapper {
+    @Mapping(source = "user.id", target = "userId")
     PostDto toDto(Post post);
 
+    @Mapping(target = "user", ignore = true)
     Post toEntity(PostDto postDto);
 }
