8d95975722a52ddf59d0177209ef2d69052206db
ramilraianov <raianov@protonmail.com>
2022-12-22 14:02:22 +0500

    Исправил пакетный импорт

--- src/main/java/com/corporation/controller/api/RoleApi.java
+++ src/main/java/com/corporation/controller/api/RoleApi.java
@@ -3,7 +3,11 @@ package com.corporation.controller.api;
 import com.corporation.dto.RoleDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/role")
 @Tag(name = "Управление ролями", description = "Создание ролей")
