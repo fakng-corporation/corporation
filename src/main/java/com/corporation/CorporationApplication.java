4c0a27719cd3ce80675acc8734ca7172f8470d4d
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-25 14:12:55 +0300

    Поправил код

--- src/main/java/com/corporation/CorporationApplication.java
+++ src/main/java/com/corporation/CorporationApplication.java
@@ -1,7 +1,15 @@
 package com.corporation;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CorporationApplication {
@@ -10,4 +18,28 @@ public class CorporationApplication {
         SpringApplication.run(CorporationApplication.class, args);
     }
 
+    @Value("${aws.credentials.aws_access_key_id}")
+    String awsAccessKey;
+
+    @Value("${aws.credentials.aws_secret_access_key}")
+    String awsSecretKey;
+
+    @Value("${aws.region}")
+    String awsRegion;
+
+    @Bean
+    public AmazonSimpleEmailService amazonSes() {
+        Regions clientRegion = Regions.valueOf(awsRegion);
+
+        AWSCredentials credentials = new BasicAWSCredentials(
+                awsAccessKey,
+                awsSecretKey
+        );
+
+        return AmazonSimpleEmailServiceClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(clientRegion).build();
+
+    }
 }
--- src/main/java/com/corporation/service/email/EmailService.java
+++ src/main/java/com/corporation/service/email/EmailService.java
@@ -1,56 +1,73 @@
 package com.corporation.service.email;
 
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 import com.amazonaws.services.simpleemail.model.Body;
 import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.MessageRejectedException;
 import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.corporation.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
  * @author Bleschunov Dmitry
  */
+@Log4j2
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
     @Value("${email.from}")
     private String from;
 
+    private final AmazonSimpleEmailService amazonSes;
+
     public void sendEmail(Email email) {
         try {
-            AmazonSimpleEmailService client =
-                    AmazonSimpleEmailServiceClientBuilder.standard()
-                            .withRegion(Regions.US_EAST_1).build();
-            SendEmailRequest request = new SendEmailRequest()
-                    .withDestination(
-                            new Destination().withToAddresses(email.getTo()))
-                    .withSource(from);
+            SendEmailRequest request = createRequest(email);
 
             if (email.getTextBody() != null) {
-                request.withMessage(new Message()
-                                .withBody(new Body()
-                                        .withText(new Content()
-                                                .withCharset("UTF-8").withData(email.getTextBody())))
-                        .withSubject(new Content()
-                                .withCharset("UTF-8").withData(email.getSubject())));
+                addTextBody(request, email);
             }
 
             if (email.getHtmlBody() != null) {
-                request.withMessage(new Message()
-                        .withBody(new Body()
-                                .withHtml(new Content()
-                                        .withCharset("UTF-8").withData(email.getHtmlBody())))
-                        .withSubject(new Content()
-                                .withCharset("UTF-8").withData(email.getSubject())));
+                addHtmlBody(request, email);
             }
 
-            client.sendEmail(request);
-        } catch (Exception e) {
-            e.printStackTrace();
+            amazonSes.sendEmail(request);
+        } catch (MessageRejectedException e) {
+            String message = "SES crashed trying to send email";
+            log.error(message, e);
+            throw new BusinessException("SES crashed trying to send email");
         }
+    }
+
+    private SendEmailRequest createRequest(Email email) {
+        return new SendEmailRequest()
+                .withDestination(
+                        new Destination().withToAddresses(email.getTo()))
+                .withSource(from);
+    }
+
+    private void addTextBody(SendEmailRequest request, Email email) {
+        request.withMessage(new Message()
+                .withBody(new Body()
+                        .withText(new Content()
+                                .withCharset("UTF-8").withData(email.getTextBody())))
+                .withSubject(new Content()
+                        .withCharset("UTF-8").withData(email.getSubject())));
+    }
+
+    private void addHtmlBody(SendEmailRequest request, Email email) {
+        request.withMessage(new Message()
+                .withBody(new Body()
+                        .withHtml(new Content()
+                                .withCharset("UTF-8").withData(email.getHtmlBody())))
+                .withSubject(new Content()
+                        .withCharset("UTF-8").withData(email.getSubject())));
     }
 }
