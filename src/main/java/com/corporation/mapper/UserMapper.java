b686215a3a889e260fa90c43bdff0e92d702a971
v.bulatov <v.bulatov@sbereducation.ru>
2023-01-03 13:36:37 +0300

    Resolving warnings

--- src/main/java/com/corporation/mapper/UserMapper.java
+++ src/main/java/com/corporation/mapper/UserMapper.java
@@ -2,8 +2,10 @@ package com.corporation.mapper;
 
 import com.corporation.dto.UserDto;
 import com.corporation.model.User;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 /**
@@ -12,7 +14,18 @@ import org.mapstruct.MappingTarget;
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.FIELD)
 public interface UserMapper {
 
+    @Mapping(target = "id", ignore = false)
+    @Mapping(target = "nickname", ignore = false)
+    @Mapping(target = "email", ignore = false)
+    @Mapping(target = "aboutMe", ignore = false)
+    @BeanMapping(ignoreByDefault = true)
     void updateEntity(UserDto userDto, @MappingTarget User user);
     UserDto toDto(User user);
+    @Mapping(target = "id", ignore = false)
+    @Mapping(target = "nickname", ignore = false)
+    @Mapping(target = "email", ignore = false)
+    @Mapping(target = "aboutMe", ignore = false)
+    @BeanMapping(ignoreByDefault = true)
+
     User toEntity(UserDto userDto);
 }
