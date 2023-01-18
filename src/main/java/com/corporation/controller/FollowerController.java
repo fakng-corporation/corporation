34073eb45fcbf41bfe0e3b322ffa3923627a4a22
v.bulatov <v.bulatov@sbereducation.ru>
2023-01-18 18:18:06 +0300

    Issue-56 Refactoring

--- src/main/java/com/corporation/controller/FollowerController.java
+++ src/main/java/com/corporation/controller/FollowerController.java
@@ -21,7 +21,7 @@ public class FollowerController {
         followerService.followProject(followingProjectId, user.getId());
     }
 
-    @PutMapping("/user/follow/{followeeId}")
+    @PutMapping("/user/{followeeId}/follow/")
     public void followUser(
             @AuthenticationPrincipal User user,
             @PathVariable("followeeId") long followeeId) {
