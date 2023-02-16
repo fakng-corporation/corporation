684764d6fdeb3029b75edd4710ae5033ebbf1b14
Dregid <ufl852@gmail.com>
2023-02-16 17:53:38 +0600

    Создал таблицу для хранения лайков и статистики поста.

---
+++ src/main/resources/db/changelog/init-post-statistics.sql
@@ -0,0 +1,6 @@
+CREATE TABLE post_statistics(
+    post_id bigint PRIMARY KEY REFERENCES post(id) ON DELETE CASCADE,
+    likes bigint DEFAULT 0,
+    views bigint DEFAULT 0,
+    comments_amount int DEFAULT 0
+)
